25eb4bc6b3fef8273ac6da9ef94acf37cf8f32b1
Shahnoza <muminovashahnoza@gmail.com>
2018-06-13 18:36:11 -0400

    print test and timestamp

--- src/test/java/com/dice/DiceJobSearch.java
+++ src/test/java/com/dice/DiceJobSearch.java
@@ -1,5 +1,6 @@
 package com.dice;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -56,5 +57,6 @@ public class DiceJobSearch {
 		}
 		
 		driver.close();
+		System.out.println("test complited" + LocalDateTime.now());
 	}
 }
